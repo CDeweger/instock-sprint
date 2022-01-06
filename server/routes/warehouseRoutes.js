bfe0ebda030b9ab6743b318e7df6591e887d361d
CDeweger <caroljydeweger@gmail.com>
2022-01-06 15:16:53 -0800

    move readfile and writefile function to the top

--- server/routes/warehouseRoutes.js
+++ server/routes/warehouseRoutes.js
@@ -4,28 +4,6 @@ const fs = require("fs");
 
 const warehouseRouter = express.Router();
 
-// update a warehouse
-warehouseRouter.patch("/:warehouseID/edit", (req, res) => {
-  const warehouseID = req.params.warehouseID;
-  let warehouseList = JSON.parse(fs.readFileSync("./data/warehouses.json"));
-  //find the warehouse to edit
-  let targetWarehouse = warehouseList.find(
-    (warehouse) => warehouse.id === warehouseID
-  );
-  if (targetWarehouse) {
-    targetWarehouse.name = req.body.name;
-    targetWarehouse.address = req.body.address;
-    targetWarehouse.city = req.body.city;
-    targetWarehouse.country = req.body.country;
-    targetWarehouse.contact = req.body.contact;
-    // contact here is an object
-    fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseList));
-    res.status(200).send(targetWarehouse);
-  } else {
-    res.status(400).json({ message: "warehouse not found" });
-  }
-});
-
 //function for read file
 const readFile = () => {
   const warehousesData = fs.readFileSync("./data/warehouses.json");
@@ -39,6 +17,31 @@ const writeFile = (warehousesData) => {
     JSON.stringify(warehousesData, null, 2)
   );
 };
+
+// update a warehouse
+warehouseRouter.patch("/:warehouseID/edit", (req, res) => {
+  const warehouseID = req.params.warehouseID;
+  let warehouseList = readFile();
+  // let warehouseList = JSON.parse(fs.readFileSync("./data/warehouses.json"));
+
+  //find the warehouse to edit
+  let targetWarehouse = warehouseList.find(
+    (warehouse) => warehouse.id === warehouseID
+  );
+  if (targetWarehouse) {
+    targetWarehouse.name = req.body.name;
+    targetWarehouse.address = req.body.address;
+    targetWarehouse.city = req.body.city;
+    targetWarehouse.country = req.body.country;
+    targetWarehouse.contact = req.body.contact;
+    // contact here is an object
+    writeFile(warehouseList);
+    // fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseList));
+    res.status(200).send(targetWarehouse);
+  } else {
+    res.status(400).json({ message: "warehouse not found" });
+  }
+});
 
 //GET /warehouse/:warehouseId
 warehouseRouter.get("/:warehouseId", (req, res) => {
