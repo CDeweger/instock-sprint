3cea74256bf4a0cd19abe05b303d1a5f8724530e
Yuxian Xu <yuxia@Mac-mini.local>
2022-01-05 17:55:48 -0800

    Create WarehouseList and WarehouseCard components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+// import packages needed for routing
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import EditWarehouse from "./pages/EditWarehouse/EditWarehouse";
+import WarehouseList from "./components/WarehouseList/WarehouseList";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    // define the routes here
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/inventory" component={InventoryPage} />
+        <Route path="/warehouse/:id/edit" component={EditWarehouse} />
+        <Route path="/warehouseList" component={WarehouseList} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/WarehouseCard/WarehouseCard.js
+++ client/src/components/WarehouseCard/WarehouseCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import '../WarehouseCard/WarehouseCard.scss';
 import DeleteIcon from '../../assets/icons/delete_outline-24px.svg';
 import EditIcon from '../../assets/icons/edit-24px.svg';
@@ -13,30 +12,30 @@ function WarehouseCard () {
                 <div className="warehouseList__card-info">
                     <h3 className="warehouseList__card-title">WAREHOUSE</h3>
                     <div className="warehouseList__card-name">
-                        <p className="warehouseList__card-text warehouseList__card-location">Name of warehouse</p>
+                        <p className="warehouseList__card-text warehouseList__card-location">Manhattan</p>
                         <img className="warehouseList__icon" src={ChevronIcon} alt="chevron icon" />
                     </div>
                 </div>
 
                 <div className="warehouseList__card-info">
-                    <h3 className="warehouseList__card-title">CONTACT ME</h3>
-                    <p className="warehouseList__card-text">Name of contact</p> 
+                    <h3 className="warehouseList__card-title">CONTACT NAME</h3>
+                    <p className="warehouseList__card-text">Parmin Aujla</p> 
                 </div>
 
                 <div className="warehouseList__card-info">
                     <h3 className="warehouseList__card-title">ADDRESS</h3>
-                    <p className="warehouseList__card-text">Address</p> 
+                    <p className="warehouseList__card-text">503 Broadway</p> 
                 </div>
 
                 <div className="warehouseList__card-info">
                     <h3 className="warehouseList__card-title">CONTACT INFORMATION</h3>
-                    <p className="warehouseList__card-text">number</p>
-                    <p className="warehouseList__card-text">email</p>
+                    <p className="warehouseList__card-text">+1 (646) 123-1234</p>
+                    <p className="warehouseList__card-text">paujla@instock.com</p>
                 </div>
             </div>
 
             <div className="warehouseList__card-action">
-                <img className="warehouseList_card-delete" src={DeleteIcon} alt="delete icom" />
+                <img className="warehouseList_card-delete" src={DeleteIcon} alt="delete icon" />
                 <img className="warehouseList_card-edit" src={EditIcon} alt="edit icon" />
             </div>
         </div>
