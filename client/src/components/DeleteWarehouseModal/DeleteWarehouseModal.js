6ea34c89973cb7e25d470f31ddc95e3cdb4a73b6
Selamawit Hailemariam <selamatwits@gmail.com>
2022-01-06 07:59:44 -0800

    Add DeleteWarehouseModal component.

---
+++ client/src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import React from 'react';
+import { Link } from "react-router-dom";
+// Need to connect to delete icon on warehouse list
+function DeleteWarehouseModal() {
+    // for now use one warehouse ID
+    const ID ="2922c286-16cd-4d43-ab98-c79f698aeab0";
+    
+    return (
+      <article>
+        <h1>Delete King West warehouse?</h1>
+        <p>
+          Please confirm that you'd like to delete the King West from list of
+          warehouses. You wouldn't be able to undo this action.
+        </p>
+        <Link to="/">
+          <button>Cancel</button>
+        </Link>
+        <button >Delete</button>
+      </article>
+    );
+}
+
+export default DeleteWarehouseModal;
