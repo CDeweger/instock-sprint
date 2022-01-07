72be9142e045a71c45e9c858ef59c2392c915b11
hnslyswhy <huanyuw01@gmail.com>
2022-01-06 17:51:24 -0800

    add styles for edit warehouse page

--- client/src/pages/EditWarehouse/EditWarehouse.js
+++ client/src/pages/EditWarehouse/EditWarehouse.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import "./EditWarehouse.scss";
 
 export default class EditWarehouse extends Component {
   constructor(props) {
@@ -50,8 +51,24 @@ export default class EditWarehouse extends Component {
         email: e.target.email.value,
       },
     };
-  };
+    console.log(warehouseUpdatedDetails);
 
+    axios
+      .patch(
+        `${process.env.REACT_APP_API_URL}/warehouse/${this.props.match.params.id}/edit`,
+        warehouseUpdatedDetails
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.error(e);
+        alert("something went wrong");
+      });
+
+    e.target.reset();
+  };
+  /* 
   componentDidMount() {
     this.getWarehouseData(this.props.match.params.id);
   }
@@ -63,165 +80,179 @@ export default class EditWarehouse extends Component {
       this.setVideos();
     }
   }
-
+ */
   render() {
     //need to delete the next two lines once the api call is hooked
     const { name, address, city, country, contact } =
       this.state.warehouseDetails;
     return (
-      <section className="edit-warehouse">
-        <div className="edit-warehouse__top">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
-              fill="#2E66E6"
-            />
-          </svg>
-          <h1 className="edit-warehouse__title">Edit Warehouse</h1>
-        </div>
-        {!this.state.warehouseDetails && <LoadingSpinner />}
-        {this.state.warehouseDetails && (
-          <form className="editWH-form" onSubmit={this.handleEditWarehouse}>
-            <div className="editWH-form__warehouse">
-              <h2 className="editWH-form__title">Warehouse Details</h2>
-              <div className="editWH-form__entries">
-                <label htmlFor="warehouse-name" className="editWH-form__label">
-                  Warehouse Name
-                </label>
-                <input
-                  id="warehouse-name"
-                  type="text"
-                  name="name"
-                  className="editWH-form__input"
-                  defaultValue={name}
-                  required
-                ></input>
+      <div className="edit-container">
+        <section className="edit-warehouse">
+          <div className="edit-warehouse__top">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
+                fill="#2E66E6"
+              />
+            </svg>
+            <h1 className="edit-warehouse__title">Edit Warehouse</h1>
+          </div>
+          {!this.state.warehouseDetails && <LoadingSpinner />}
+          {this.state.warehouseDetails && (
+            <form className="editWH-form" onSubmit={this.handleEditWarehouse}>
+              <div className="editWH-form__warehouse">
+                <h2 className="editWH-form__title">Warehouse Details</h2>
+                <div className="editWH-form__entries">
+                  <label
+                    htmlFor="warehouse-name"
+                    className="editWH-form__label"
+                  >
+                    Warehouse Name
+                  </label>
+                  <input
+                    id="warehouse-name"
+                    type="text"
+                    name="name"
+                    className="editWH-form__input"
+                    defaultValue={name}
+                    required
+                  ></input>
+                </div>
+
+                <div className="editWH-form__entries">
+                  <label
+                    htmlFor="warehouse-street"
+                    className="editWH-form__label"
+                  >
+                    Street Address
+                  </label>
+                  <input
+                    id="warehouse-street"
+                    type="text"
+                    name="address"
+                    className="editWH-form__input"
+                    defaultValue={address}
+                    required
+                  ></input>
+                </div>
+
+                <div className="editWH-form__entries">
+                  <label
+                    htmlFor="warehouse-city"
+                    className="editWH-form__label"
+                  >
+                    City
+                  </label>
+                  <input
+                    id="warehouse-city"
+                    type="text"
+                    name="city"
+                    className="editWH-form__input"
+                    defaultValue={city}
+                    required
+                  ></input>
+                </div>
+
+                <div className="editWH-form__entries">
+                  <label
+                    htmlFor="warehouse-country"
+                    className="editWH-form__label"
+                  >
+                    Warehouse Name
+                  </label>
+                  <input
+                    id="warehouse-country"
+                    type="text"
+                    name="country"
+                    className="editWH-form__input"
+                    defaultValue={country}
+                    required
+                  ></input>
+                </div>
               </div>
 
-              <div className="editWH-form__entries">
-                <label
-                  htmlFor="warehouse-street"
-                  className="editWH-form__label"
-                >
-                  Street Address
-                </label>
-                <input
-                  id="warehouse-street"
-                  type="text"
-                  name="address"
-                  className="editWH-form__input"
-                  defaultValue={address}
-                  required
-                ></input>
+              <div className="editWH-form__contact">
+                <h2 className="editWH-form__title">Contact Details</h2>
+                <div className="editWH-form__entries">
+                  <label htmlFor="contact-name" className="editWH-form__label">
+                    Contact Name
+                  </label>
+                  <input
+                    id="contact-name"
+                    type="text"
+                    name="contactName"
+                    className="editWH-form__input"
+                    defaultValue={contact.name}
+                    required
+                  ></input>
+                </div>
+
+                <div className="editWH-form__entries">
+                  <label
+                    htmlFor="contact-position"
+                    className="editWH-form__label"
+                  >
+                    Position
+                  </label>
+                  <input
+                    id="contact-position"
+                    type="text"
+                    name="position"
+                    className="editWH-form__input"
+                    defaultValue={contact.position}
+                    required
+                  ></input>
+                </div>
+
+                <div className="editWH-form__entries">
+                  <label htmlFor="contact-phone" className="editWH-form__label">
+                    Phone Number
+                  </label>
+                  <input
+                    id="contact-phone"
+                    type="text"
+                    name="phone"
+                    className="editWH-form__input"
+                    defaultValue={contact.phone}
+                    required
+                  ></input>
+                </div>
+
+                <div className="editWH-form__entries">
+                  <label htmlFor="contact-email" className="editWH-form__label">
+                    Email
+                  </label>
+                  <input
+                    id="contact-email"
+                    type="email"
+                    name="email"
+                    className="editWH-form__input"
+                    defaultValue={contact.email}
+                    required
+                  ></input>
+                </div>
               </div>
 
-              <div className="editWH-form__entries">
-                <label htmlFor="warehouse-city" className="editWH-form__label">
-                  City
-                </label>
-                <input
-                  id="warehouse-city"
-                  type="text"
-                  name="city"
-                  className="editWH-form__input"
-                  defaultValue={city}
-                  required
-                ></input>
+              <div className="editWH-form__buttons">
+                <div className="editWH-form__buttons-container">
+                  <button type="button" id="cancel">
+                    Cancel
+                  </button>
+                  <button type="submit" id="save">
+                    Save
+                  </button>
+                </div>
               </div>
-
-              <div className="editWH-form__entries">
-                <label
-                  htmlFor="warehouse-country"
-                  className="editWH-form__label"
-                >
-                  Warehouse Name
-                </label>
-                <input
-                  id="warehouse-country"
-                  type="text"
-                  name="country"
-                  className="editWH-form__input"
-                  defaultValue={country}
-                  required
-                ></input>
-              </div>
-            </div>
-
-            <div className="editWH-form__contact">
-              <h2 className="editWH-form__title">Contact Details</h2>
-              <div className="editWH-form__entries">
-                <label htmlFor="contact-name" className="editWH-form__label">
-                  Contact Name
-                </label>
-                <input
-                  id="contact-name"
-                  type="text"
-                  name="contactName"
-                  className="editWH-form__input"
-                  defaultValue={contact.name}
-                  required
-                ></input>
-              </div>
-
-              <div className="editWH-form__entries">
-                <label
-                  htmlFor="contact-position"
-                  className="editWH-form__label"
-                >
-                  Position
-                </label>
-                <input
-                  id="contact-position"
-                  type="text"
-                  name="position"
-                  className="editWH-form__input"
-                  defaultValue={contact.position}
-                  required
-                ></input>
-              </div>
-
-              <div className="editWH-form__entries">
-                <label htmlFor="contact-phone" className="editWH-form__label">
-                  Phone Number
-                </label>
-                <input
-                  id="contact-phone"
-                  type="text"
-                  name="phone"
-                  className="editWH-form__input"
-                  defaultValue={contact.phone}
-                  required
-                ></input>
-              </div>
-
-              <div className="editWH-form__entries">
-                <label htmlFor="contact-email" className="editWH-form__label">
-                  Email
-                </label>
-                <input
-                  id="contact-email"
-                  type="email"
-                  name="email"
-                  className="editWH-form__input"
-                  defaultValue={contact.email}
-                  required
-                ></input>
-              </div>
-            </div>
-
-            <div>
-              <button type="button">Cancel</button>
-              <button type="submit">Save</button>
-            </div>
-          </form>
-        )}
-      </section>
+            </form>
+          )}
+        </section>
+      </div>
     );
   }
 }
