b12f649ef64372f514790d0a233169aebf33bbc4
Selamawit Hailemariam <selamatwits@gmail.com>
2022-01-06 22:42:08 -0800

    Add onClick function to make api call to delete warehouse.

--- client/src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
+++ client/src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
@@ -3,8 +3,14 @@ import React from 'react';
 import { Link } from "react-router-dom";
 // Need to connect to delete icon on warehouse list and also add onclick functionality to delete button.
 function DeleteWarehouseModal() {
-    // for now use one warehouse ID
+    // for now use one warehouse ID, for future this we get from props
     const ID ="2922c286-16cd-4d43-ab98-c79f698aeab0";
+    const handleDelete=(ID)=>{
+      axios.get(`http://localhost:3000/warehouse/${ID}/edit`)
+      .then(resolve=>console.log(resolve.data))
+      .catch(error=>error);
+
+    }
     
     return (
       <article>
@@ -16,7 +22,7 @@ function DeleteWarehouseModal() {
         <Link to="/">
           <button>Cancel</button>
         </Link>
-        <button >Delete</button>
+        <button onClick={handleDelete(ID)}>Delete</button>
       </article>
     );
 }
