841ff0180d6d1b4c70312610dc181f6fcc90e050
Yuxian Xu <yuxia@Mac-mini.local>
2022-01-06 21:36:04 -0800

    Create handle event for delet warehouse

--- client/src/components/WarehouseCard/WarehouseCard.js
+++ client/src/components/WarehouseCard/WarehouseCard.js
@@ -1,55 +1,108 @@
 import React from "react";
-import '../WarehouseCard/WarehouseCard.scss';
-import DeleteIcon from '../../assets/icons/delete_outline-24px.svg';
-import EditIcon from '../../assets/icons/edit-24px.svg';
-import ChevronIcon from '../../assets/icons/chevron_right-24px.svg';
+import "../WarehouseCard/WarehouseCard.scss";
+import DeleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import EditIcon from "../../assets/icons/edit-24px.svg";
+import ChevronIcon from "../../assets/icons/chevron_right-24px.svg";
+import axios from "axios";
+import DeleteWarehouseModal from "../DeleteWarehouseModal/DeleteWarehouseModal";
+const { REACT_APP_API_URL } = process.env;
 
-function WarehouseCard () {
+class WarehouseCard extends React.Component {
+  state = {
+    showModal: false,
+  };
+
+  showModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  handleDelete() {
+    axios
+      .delete(`${REACT_APP_API_URL}/warehouse/${this.props.id}`)
+      .then((res) => {
+        this.setState({
+          showModal: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    let modal = <></>;
+
+    if (this.state.showModal) {
+      modal = (
+        <DeleteWarehouseModal
+          closeModal={this.closeModal}
+          delete={() => this.handleDelete()}
+        />
+      );
+    }
+
     return (
-        <>
+      <>
         <div className="warehouseList__card">
-            <div className="warehouseList__card-container">
-                <div className="warehouseList__card-info">
-                    <h3 className="warehouseList__card-title">WAREHOUSE</h3>
-                    <div className="warehouseList__card-name">
-                        <p className="warehouseList__card-text warehouseList__card-location">Manhattan</p>
-                        <img className="warehouseList__icon" src={ChevronIcon} alt="chevron icon" />
-                    </div>
+          <div className="warehouseList__card-container">
+            <div className="warehouseList__card-info">
+              <h3 className="warehouseList__card-title">WAREHOUSE</h3>
+              <Link
+                to={`/warehouse/${this.props.id}`}
+                className="warehouseList__link"
+              >
+                <div className="warehouseList__card-name">
+                  <p className="warehouseList__card-text warehouseList__card-location">
+                    Manhattan
+                  </p>
+                  <img
+                    className="warehouseList__icon"
+                    src={ChevronIcon}
+                    alt="chevron icon"
+                  />
                 </div>
-
-                <div className="warehouseList__card-info">
-<<<<<<< HEAD
-                    <h3 className="warehouseList__card-title">CONTACT NAME</h3>
-=======
-                    <h3 className="warehouseList__card-title">CONTACT ME</h3>
->>>>>>> O21DM2-6-AddWarehouseComponent
-                    <p className="warehouseList__card-text">Parmin Aujla</p> 
-                </div>
-
-                <div className="warehouseList__card-info">
-                    <h3 className="warehouseList__card-title">ADDRESS</h3>
-<<<<<<< HEAD
-                    <p className="warehouseList__card-text">503 Broadway</p> 
-=======
-                    <p className="warehouseList__card-text">503 Broadway, New York USA"</p> 
->>>>>>> O21DM2-6-AddWarehouseComponent
-                </div>
-
-                <div className="warehouseList__card-info">
-                    <h3 className="warehouseList__card-title">CONTACT INFORMATION</h3>
-                    <p className="warehouseList__card-text">+1 (646) 123-1234</p>
-                    <p className="warehouseList__card-text">paujla@instock.com</p>
-                </div>
+              </Link>
             </div>
 
-            <div className="warehouseList__card-action">
-                <img className="warehouseList_card-delete" src={DeleteIcon} alt="delete icon" />
-                <img className="warehouseList_card-edit" src={EditIcon} alt="edit icon" />
+            <div className="warehouseList__card-info">
+              <h3 className="warehouseList__card-title">CONTACT NAME</h3>
+              <p className="warehouseList__card-text">Parmin Aujla</p>
             </div>
+
+            <div className="warehouseList__card-info">
+              <h3 className="warehouseList__card-title">ADDRESS</h3>
+              <p className="warehouseList__card-text">
+                503 Broadway, New York USA"
+              </p>
+            </div>
+
+            <div className="warehouseList__card-info">
+              <h3 className="warehouseList__card-title">CONTACT INFORMATION</h3>
+              <p className="warehouseList__card-text">+1 (646) 123-1234</p>
+              <p className="warehouseList__card-text">paujla@instock.com</p>
+            </div>
+          </div>
+
+          <div className="warehouseList__card-action">
+            <img
+              className="warehouseList_card-delete"
+              src={DeleteIcon}
+              alt="delete icon"
+            />
+            <img
+              className="warehouseList_card-edit"
+              src={EditIcon}
+              alt="edit icon"
+            />
+          </div>
         </div>
-        
-        </>
-    )
+      </>
+    );
+  }
 }
 
 export default WarehouseCard;
