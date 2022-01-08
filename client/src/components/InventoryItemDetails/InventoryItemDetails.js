9c658bd56b1449383b1ba39a3aa53148f9507c49
CDeweger <caroljydeweger@gmail.com>
2022-01-08 13:16:42 -0800

    update mobile styling

--- client/src/components/InventoryItemDetails/InventoryItemDetails.js
+++ client/src/components/InventoryItemDetails/InventoryItemDetails.js
@@ -9,34 +9,50 @@ const InventoryItemDetails = () => {
     <>
       <div className="item-container">
         <div className="item-container__header">
-          <img src={arrowBackImg} alt="arrow-back"></img>
+          <Link to={`/`} className="item-container__link-back">
+            <img src={arrowBackImg} alt="arrow-back"></img>
+          </Link>
           <h1 className="item-container__title">Television</h1>
           <Link to={`/`}>
             <img src={editImg} alt="edit"></img>Edit
           </Link>
         </div>
-        <div className="item-info">
-          <p className="item-info__description">ITEM DESCRIPTION</p>
-          <p>
-            This 50", 4K LED TV provides a crystal-clear picture and vivid
-            colors.
-          </p>
-          <p>CATEGORY</p>
-          <p>Electronics</p>
-        </div>
-        <div className="stock-status">
-          <div>
-            <p>STATUS</p>
-            <p>IN STOCK</p>
+        <div className="item-box">
+          <div className="item-info">
+            <p className="item-info__title item-info__title-description">
+              ITEM DESCRIPTION:
+            </p>
+            <p className="item-info__detail">
+              This 50", 4K LED TV provides a crystal-clear picture and vivid
+              colors.
+            </p>
+            <p className="item-info__title item-info__title-category">
+              CATEGORY:
+            </p>
+            <p className="item-info__detail">Electronics</p>
           </div>
-          <div>
-            <p>QUANTITY</p>
-            <p>500</p>
+
+          <div className="stock-info">
+            <div className="stock-info__status-qty">
+              <p className="item-info__title item-info__title-status">
+                STATUS:
+              </p>
+              <p className="item-info__detail-status">IN STOCK</p>
+            </div>
+            <div>
+              <p className="item-info__title item-info__title-quantity">
+                QUANTITY:
+              </p>
+              <p className="item-info__detail">500</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <p>WAREHOUSE:</p>
-          <p>Manhattan</p>
+
+          <div className="warehouse-info">
+            <p className="item-info__title item-info__title-warehouse">
+              WAREHOUSE:
+            </p>
+            <p className="item-info__detail">Manhattan</p>
+          </div>
         </div>
       </div>
     </>
