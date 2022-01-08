17e1fe3e128ea321e0fb2e3ea61eb2484739b1f8
CDeweger <caroljydeweger@gmail.com>
2022-01-08 14:00:06 -0800

    commit mobile styling

--- client/src/components/InventoryItemDetails/InventoryItemDetails.js
+++ client/src/components/InventoryItemDetails/InventoryItemDetails.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./InventoryItemDetails.scss";
 import editImg from "../../assets/icons/edit-24px.svg";
-import arrowBackImg from "../../assets/icons/arrow_back-24px.svg";
+//import arrowBackImg from "../../assets/icons/arrow_back-24px.svg";
 
 const InventoryItemDetails = () => {
   return (
@@ -10,12 +10,41 @@ const InventoryItemDetails = () => {
       <div className="item-container">
         <div className="item-container__header">
           <Link to={`/`} className="item-container__link-back">
-            <img src={arrowBackImg} alt="arrow-back"></img>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
+                fill="#2E66E6"
+              />
+            </svg>
+            {/* <img src={arrowBackImg} alt="arrow-back"></img> */}
           </Link>
           <h1 className="item-container__title">Television</h1>
-          <Link to={`/`}>
-            <img src={editImg} alt="edit"></img>Edit
-          </Link>
+
+          <div className="item-container__edit-background">
+            <Link to={`/`}>
+              <svg
+                className="item-container__edit-img"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="#FFFFFF"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
+                  fill="#FFFFFF"
+                />
+              </svg>
+
+              {/* <img src={editImg} alt="edit"></img>Edit */}
+            </Link>
+          </div>
         </div>
         <div className="item-box">
           <div className="item-info">
