27b2ff08b5a96c76147dfd3bdeb240921f20eec1
hnslyswhy <huanyuw01@gmail.com>
2022-01-10 14:52:57 -0800

    fix back button in edit warehouse page

--- client/src/pages/EditWarehouse/EditWarehouse.js
+++ client/src/pages/EditWarehouse/EditWarehouse.js
@@ -173,7 +173,7 @@ export default class EditWarehouse extends Component {
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              onChange={this.handleCancel}
+              onClick={this.handleCancel}
             >
               <path
                 d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
