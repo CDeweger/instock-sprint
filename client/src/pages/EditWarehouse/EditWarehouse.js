c4fe7d1b68791a76c70e0ee2a6316dd7ae3012df
hnslyswhy <huanyuw01@gmail.com>
2022-01-10 16:59:52 -0800

    add cursor to edit warehouse back icon, add animation to warehouse page

--- client/src/pages/EditWarehouse/EditWarehouse.js
+++ client/src/pages/EditWarehouse/EditWarehouse.js
@@ -173,8 +173,8 @@ export default class EditWarehouse extends Component {
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="edit-warehouse__back-icon"
               onClick={this.handleCancel}
+              className="edit-warehouse__back-icon"
             >
               <path
                 d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
